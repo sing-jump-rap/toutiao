bc83a74c9ab007d28e4d6cb63b579b864e4a9273
MaTeng <1749156618@qq.com>
2019-09-18 21:47:41 +0800

    04-登录基本校验完成

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -4,19 +4,20 @@
           <div class="title">
               <img src="../../assets/img/logo_index.png" alt="">
           </div>
-          <el-form style="margin-top:20px">
-              <el-form-item>
+          <!-- 数据校验 el-from绑定model，绑定rules规则 -->
+          <el-form ref="myForm" :model="loginFrom" :rules="loginRules" style="margin-top:20px">
+              <el-form-item prop="mobile">
                   <!-- 手机号 -->
-                  <el-input></el-input>
+                  <el-input v-model="loginFrom.mobile" placeholder="输入手机号！！"></el-input>
               </el-form-item>
 
-              <el-form-item>
+              <el-form-item prop="code">
                   <!-- 验证码 -->
-                  <el-input style="width:65%"></el-input>
-                  <el-button style="float:right">发送验证码</el-button>
+                  <el-input v-model="loginFrom.code" style="width:65%" placeholder="输入验证码！！"></el-input>
+                  <el-button  style="float:right">发送验证码</el-button>
               </el-form-item>
-              <el-form-item>
-                  <el-checkbox>我已阅读还同意条款了</el-checkbox>
+              <el-form-item prop="agree">
+                  <el-checkbox v-model="loginFrom.agree">我已阅读还同意条款了</el-checkbox>
               </el-form-item>
               <el-form-item>
                   <el-button type="primary" style="width:100%">登录</el-button>
@@ -28,7 +29,34 @@
 
 <script>
 export default {
-
+  data () {
+    return {
+      // 数据对象
+      loginFrom: {
+        mobile: '', // 手机号
+        code: '', // 验证码
+        agree: false// 是否同意协议
+      },
+      // 校验规则  key(字段名)：value（对象数组）=> 一个对象就是一个校验规则
+      // required为true表示为必填项，如不填就会提示message的消息
+      loginRules: {
+        mobile: [{ required: true, message: '请输入宁的手机号' }],
+        code: [{ required: true, message: '请输入宁的验证码' }],
+        agree: []
+      }
+    }
+  },
+  methods: {
+    login () {
+      // validate方法，方法中传入一个函数 俩个校验参数 是否校验成功/为校验成功的子段
+      this.$refs.myForm.validate(function (isOK) {
+        if (isOK) {
+          debugger
+          console.log('校验成功')
+        }
+      })
+    }
+  }
 }
 </script>
 
