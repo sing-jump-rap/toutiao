42a2bb37a950f86fefe313748d09a6c70a7af8ba
MaTeng <1749156618@qq.com>
2019-09-19 15:15:49 +0800

    09-主页导航样式完成

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,10 +1,26 @@
 <template>
-  <div>我是主页</div>
+<!-- 大容器 -->
+<el-container>
+  <el-aside>
+    <!-- 使用左导航组件 -->
+    <layout-aside></layout-aside>
+  </el-aside>
+  <el-container>
+    <el-header>头</el-header>
+    <el-main>
+      <!-- 二级路由的容器 -->
+      <router-view></router-view>
+    </el-main>
+  </el-container>
+</el-container>
 </template>
 
 <script>
+import layoutAside from '../../components/home/layout-aside'
 export default {
-
+  components: {
+    'layout-aside': layoutAside
+  }
 }
 </script>
 
