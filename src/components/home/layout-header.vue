9c6b00f57a90e94dfec86530a436aa4555547715
MaTeng <1749156618@qq.com>
2019-09-20 19:01:16 +0800

    10-头部样式完成

---
+++ src/components/home/layout-header.vue
@@ -0,0 +1,49 @@
+<template>
+  <el-row align="middle" class="layout-header" type="flex" justify="space-between">
+    <el-col :span="6">
+      <!-- 左侧 -->
+      <i class="el-icon-s-unfold"></i>
+      <span class="title">马氏网络科技股份有限公司</span>
+    </el-col>
+    <el-col :span="4">
+      <!-- 右侧 -->
+      <img class="head-img" src="../../assets/img/avatar.jpg" alt="" style="margin-right:5px">
+      <el-dropdown>
+        <span class="el-dropdown-link">
+          鹅？<i class="el-icon-arrow-down el-icon--right"></i>
+        </span>
+        <el-dropdown-menu slot="dropdown">
+          <el-dropdown-item>个人信息</el-dropdown-item>
+          <el-dropdown-item>git地址</el-dropdown-item>
+          <el-dropdown-item>退出</el-dropdown-item>
+        </el-dropdown-menu>
+      </el-dropdown>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+  .layout-header{
+    height: 50px;
+    .el-icon-s-unfold {
+      font-size: 22px;
+    }
+    .title {
+      vertical-align: top;
+      margin-left: 4px;
+    }
+    .head-img {
+      width: 40px;
+      height: 40px;
+      border-radius: 50%;
+      vertical-align: middle;
+      margin-top: 4px;
+    }
+  }
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,12 +1,15 @@
 <template>
 <!-- 大容器 -->
 <el-container>
-  <el-aside>
+  <el-aside style="width:200px">
     <!-- 使用左导航组件 -->
     <layout-aside></layout-aside>
   </el-aside>
   <el-container>
-    <el-header>头</el-header>
+    <el-header>
+    <!-- 头组件 -->
+    <layout-header></layout-header>
+    </el-header>
     <el-main>
       <!-- 二级路由的容器 -->
       <router-view></router-view>
@@ -17,9 +20,11 @@
 
 <script>
 import layoutAside from '../../components/home/layout-aside'
+import layoutHeader from '../../components/home/layout-header'
 export default {
   components: {
-    'layout-aside': layoutAside
+    'layout-aside': layoutAside,
+    'layout-header': layoutHeader
   }
 }
 </script>
