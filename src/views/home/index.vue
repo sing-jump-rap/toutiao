03c561e94c73bf5967115e10c9cac840888bdf37
MaTeng <1749156618@qq.com>
2019-09-20 22:11:14 +0800

    15-默认导航

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,7 +1,7 @@
 <template>
 <!-- 大容器 -->
 <el-container>
-  <el-aside style="width:200px">
+  <el-aside style="width:200px;overflow:hidden;background-color:#323745">
     <!-- 使用左导航组件 -->
     <layout-aside></layout-aside>
   </el-aside>
@@ -10,7 +10,7 @@
     <!-- 头组件 -->
     <layout-header></layout-header>
     </el-header>
-    <el-main>
+    <el-main style="padding:0">
       <!-- 二级路由的容器 -->
       <router-view></router-view>
     </el-main>
---
+++ src/views/home/main.vue
@@ -0,0 +1,18 @@
+<template>
+    <!-- 一级路由下默认的二级路由 -->
+  <div class="home"></div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+    .home {
+        background-image: url('../../assets/img/moren.jpg');
+        height: 100vh;
+        background-size:cover;
+    }
+</style>
