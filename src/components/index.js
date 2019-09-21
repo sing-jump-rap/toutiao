571835302f63f566558a626d59be6334bd963cda
MaTeng <1749156618@qq.com>
2019-09-21 19:07:57 +0800

    20-面包屑封装

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,12 @@
 // 全局注册组件
 import layoutAside from './home/layout-aside'
 import layoutHeade from './home/layout-header'
+import breadCrumb from './common/bread-crumb'
 
 export default {
   install (Vue) {
     Vue.component('layout-aside', layoutAside)
     Vue.component('layout-header', layoutHeade)
+    Vue.component('bread-crumb', breadCrumb)
   }
 }
