d9a179288132fc5142b57ec2b2e41473a6eb454d
MaTeng <1749156618@qq.com>
2019-09-25 22:01:23 +0800

    账户信息数据加载

--- src/views/account/index.vue
+++ src/views/account/index.vue
@@ -6,27 +6,46 @@
         <!-- 表单 -->
         <el-form style='margin-left:60px' label-width="100px">
             <el-form-item label="用户名">
-                <el-input style="width:300px"></el-input>
+                <el-input v-model="formData.name" style="width:300px"></el-input>
             </el-form-item>
             <el-form-item label="简介">
-                <el-input style="width:300px"></el-input>
+                <el-input v-model="formData.intro" style="width:300px"></el-input>
             </el-form-item>
             <el-form-item label="邮箱">
-                <el-input style="width:300px"></el-input>
+                <el-input v-model="formData.email" style="width:300px"></el-input>
             </el-form-item>
             <el-form-item label="手机号">
-                <el-input style="width:300px"></el-input>
+                <el-input v-model="formData.mobile" style="width:300px"></el-input>
             </el-form-item>
             <el-form-item>
                 <el-button type='primary'>保存信息</el-button>
             </el-form-item>
         </el-form>
-        <img class='head-img' src="../../assets/img/abb.jpg" alt="">
+        <img class='head-img' :src="formData.photo?formData.photo:defaultImg" alt="">
     </el-card>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      defaultImg: require('../../assets/img/abb.jpg'),
+      formData: {}
+    }
+  },
+  methods: {
+    // 获取用户个人信息
+    getUserInfo () {
+      this.$axios({
+        url: '/user/profile'
+      }).then((result) => {
+        this.formData = result.data
+      })
+    }
+  },
+  created () {
+    this.getUserInfo()
+  }
 }
 </script>
 
