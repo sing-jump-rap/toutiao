848cfc5186c27562011a47d551506513617d1d21
MaTeng <1749156618@qq.com>
2019-09-25 18:03:44 +0800

    发布文章注册使用富文本编辑器

--- src/components/index.js
+++ src/components/index.js
@@ -2,11 +2,17 @@
 import layoutAside from './home/layout-aside'
 import layoutHeade from './home/layout-header'
 import breadCrumb from './common/bread-crumb'
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+
+import { quillEditor } from 'vue-quill-editor'
 
 export default {
   install (Vue) {
-    Vue.component('layout-aside', layoutAside)
-    Vue.component('layout-header', layoutHeade)
-    Vue.component('bread-crumb', breadCrumb)
+    Vue.component('layout-aside', layoutAside)// zuoce
+    Vue.component('layout-header', layoutHeade)// 头部
+    Vue.component('bread-crumb', breadCrumb)// 面包屑
+    Vue.component('quill-editor', quillEditor) // 全局注册富文本编辑
   }
 }
